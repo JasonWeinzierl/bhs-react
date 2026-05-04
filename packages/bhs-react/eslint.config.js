615e7c4ce4709678da0526d8935e863c6e75b93e
Jason Weinzierl <weinzierljason@gmail.com>
2026-05-03 22:28:42 -0500

    refactor: use tailwindcss via uniwind

--- packages/bhs-react/eslint.config.js
+++ packages/bhs-react/eslint.config.js
@@ -1,10 +1,24 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const tailwindcss = require('eslint-plugin-better-tailwindcss');
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+  },
+  {
+    extends: [
+      tailwindcss.configs.recommended,
+    ],
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: './global.css',
+      },
+    },
+    rules: {
+      'better-tailwindcss/enforce-consistent-line-wrapping': 'off',
+    },
   },
 ]);
