a96833c81cecb77d4eeb096f2fbe788060297b70
Jason Weinzierl <weinzierljason@gmail.com>
2026-05-09 13:43:12 -0500

    chore(e2e): ability to pick browser

--- packages/bhs-e2e-web/wdio.conf.ts
+++ packages/bhs-e2e-web/wdio.conf.ts
@@ -1,39 +1,66 @@
 import * as core from '@actions/core';
 
+const ALL_CAPABILITIES = [{
+  browserName: 'chrome',
+  'goog:chromeOptions': {
+    args: [
+      'headless',
+      'window-size=1920,1080',
+    ],
+  },
+}, {
+  browserName: 'firefox',
+  'moz:firefoxOptions': {
+    args: [
+      '-headless',
+    ],
+  },
+}, {
+  browserName: 'safari',
+}, {
+  browserName: 'MicrosoftEdge',
+  'ms:edgeOptions': {
+    args: [
+      'headless',
+      'window-size=1920,1080',
+    ],
+  },
+}];
+
+function getCapabilities() {
+  if (process.env.CI) {
+    if (core.platform.isMacOS) {
+      return ALL_CAPABILITIES;
+    } else {
+      core.warning(`Safari is not available on ${core.platform.platform}. Removing Safari capabilities.`);
+      return ALL_CAPABILITIES.filter(cap => cap.browserName !== 'safari');
+    }
+  } else if (process.env.BROWSERS) {
+    const requestedBrowsers = new Set(process.env.BROWSERS
+      .split(',')
+      .map(browser => browser.trim().toLowerCase())
+      .map(browser => browser === 'edge' ? 'MicrosoftEdge' : browser));
+    return ALL_CAPABILITIES.filter(cap => requestedBrowsers.has(cap.browserName));
+  } else if (process.env.BROWSER) {
+    const browser = process.env.BROWSER.trim().toLowerCase();
+    const mappedBrowser = browser === 'edge' ? 'MicrosoftEdge' : browser;
+    const cap = ALL_CAPABILITIES.find(cap => cap.browserName === mappedBrowser);
+    if (!cap) throw new Error(`Unsupported browser specified in BROWSER environment variable: ${process.env.BROWSER}`);
+    return [cap];
+  } else {
+    return [ALL_CAPABILITIES[0]];
+  }
+}
+
 export const config: WebdriverIO.Config = {
   specs: [
     './test/specs/**/*.ts',
   ],
 
-  baseUrl: 'https://localhost:8081',
+  baseUrl: 'http://localhost:8081',
 
   maxInstances: 10,
-  capabilities: [{
-    browserName: 'chrome',
-    'goog:chromeOptions': {
-      args: [
-        'headless',
-        'window-size=1920,1080',
-      ],
-    },
-  }, {
-    browserName: 'firefox',
-    'moz:firefoxOptions': {
-      args: [
-        '-headless',
-      ],
-    },
-  }, {
-    browserName: 'safari',
-  }, {
-    browserName: 'MicrosoftEdge',
-    'ms:edgeOptions': {
-      args: [
-        'headless',
-        'window-size=1920,1080',
-      ],
-    },
-  }],
+  capabilities: getCapabilities(),
 
   logLevel: 'info',
   bail: 0,
@@ -46,13 +73,5 @@ export const config: WebdriverIO.Config = {
   framework: 'jasmine',
   jasmineOpts: {
     defaultTimeoutInterval: 60_000,
-  },
-
-  onPrepare(_cfg, capabilities) {
-    if (!core.platform.isMacOS && Array.isArray(capabilities)) {
-      core.warning(`Safari is not available on ${core.platform.platform}. Removing Safari capabilities.`);
-      const safariIndex = capabilities.findIndex(cap => 'browserName' in cap && cap.browserName === 'safari');
-      capabilities.splice(safariIndex, 1);
-    }
   },
 };
