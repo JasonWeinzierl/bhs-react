ecbdaeadef8a9ac21f387e76b614a8b2d7cbdaf0
Jason Weinzierl <weinzierljason@gmail.com>
2026-05-10 22:54:09 -0500

    chore: add todo for route loading indicator

--- packages/bhs-react/app/_layout.tsx
+++ packages/bhs-react/app/_layout.tsx
@@ -1,5 +1,15 @@
 import '../global.css';
+
 import { Stack } from 'expo-router';
+
+// TODO: route loading is customization is unreleased: https://github.com/expo/expo/pull/43885
+// export function SuspenseFallback() {
+//  return (
+//    <View className="flex-1 items-center justify-center">
+//      <ActivityIndicator size="large" />
+//    </View>
+//  );
+// }
 
 export default function RootLayout() {
   return <Stack />;
