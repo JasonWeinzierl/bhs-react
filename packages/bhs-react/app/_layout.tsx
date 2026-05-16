38f7e0b7c5f3696aa7806488b97949ca9e40c036
Jason Weinzierl <weinzierljason@gmail.com>
2026-05-16 00:37:56 -0500

    feat: wood background pattern

--- packages/bhs-react/app/_layout.tsx
+++ packages/bhs-react/app/_layout.tsx
@@ -1,6 +1,7 @@
 import '../global.css';
 
 import { Stack } from 'expo-router';
+import { ImageBackground } from 'react-native';
 
 // TODO: route loading is customization is unreleased: https://github.com/expo/expo/pull/43885
 // export function SuspenseFallback() {
@@ -12,5 +13,19 @@ import { Stack } from 'expo-router';
 // }
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ImageBackground
+      source={{ uri: 'https://beltonhistoricalsociety.org/assets/img/2019/webtreats_wood-pattern3-512.jpg' }}
+      resizeMode="repeat"
+      className="flex-1"
+      width={512}
+      height={512}
+    >
+      <Stack
+        screenOptions={{
+          presentation: 'transparentModal',
+        }}
+      />
+    </ImageBackground>
+  );
 }
