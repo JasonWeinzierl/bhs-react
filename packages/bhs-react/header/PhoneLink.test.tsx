7b77959a11b66b632b930f2bd3c44c245ea41b67
Jason Weinzierl <weinzierljason@gmail.com>
2026-06-03 13:51:07 -0500

    test: coverage of phone link click

--- packages/bhs-react/header/PhoneLink.test.tsx
+++ packages/bhs-react/header/PhoneLink.test.tsx
@@ -1,6 +1,11 @@
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
+import * as Linking from 'expo-linking';
 
 import PhoneLink from './PhoneLink';
+
+jest.mock('expo-linking', () => ({
+  openURL: jest.fn().mockResolvedValue(true),
+}));
 
 describe('PhoneLink', () => {
   it('renders the display text', () => {
@@ -12,5 +17,18 @@ describe('PhoneLink', () => {
     );
 
     expect(getByText('555-555-5555')).toBeTruthy();
+  });
+
+  it('opens the URL when pressed', () => {
+    const { getByText } = render(
+      <PhoneLink
+        url="tel:5555555555"
+        display="555-555-5555"
+      />,
+    );
+
+    fireEvent.press(getByText('555-555-5555'));
+
+    expect(Linking.openURL).toHaveBeenCalledWith('tel:5555555555');
   });
 });
